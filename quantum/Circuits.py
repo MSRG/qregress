9c7732b3ade81ee6a35b4f373d217d0949b31644
taylo773 <nick.taylor@mail.utoronto.ca>
2023-05-31 10:20:37 -0400

    Updated Files

--- quantum/Circuits.py
+++ quantum/Circuits.py
@@ -1,42 +1,61 @@
 import pennylane as qml
 from pennylane import numpy as np
+import math
 
 
-def mitarai(features):
+def mitarai(features, wires):
     for i in range(len(features)):
-        qml.RY(np.arcsin(features[i]), wires=i)
-        qml.RZ(np.arccos(features[i]**2), wires=i)
+        qml.RY(np.arcsin(features[i]), wires=wires[i])
+        qml.RZ(np.arccos(features[i]**2), wires=wires[i])
 
 
-def single_angle(features):
-    for i in range(len(features)):
-        qml.RY(features[i], wires=i)
+def single_angle(features, wires):
+    if len(features) >> len(wires):
+        raise ValueError("Cannot encode more features than there are wires")
+    for j in range(math.floor(len(wires) / len(features))):
+        for i in range(len(features)):
+            qml.RY(features[i], wires=wires[i + j * math.floor(len(wires) / len(features))])
 
 
-def double_angle(features):
-    for i in range(len(features)):
-        qml.RY(features[i], wires=i)
-        qml.RZ(features[i], wires=i)
+def double_angle(features, wires):
+    if len(features) >> len(wires):
+        raise ValueError("Cannot encode more features than there are wires")
+    for j in range(math.floor(len(wires) / len(features))):
+        for i in range(int(len(features)/2)):
+            qml.RY(features[i], wires=wires[i + j * math.floor(len(wires) / len(features))])
+            qml.RZ(features[i + 1], wires=wires[i + j * math.floor(len(wires) / len(features))])
 
 
-def entangle_cnot(num_qubits):
-    for i in range(num_qubits):
-        if i is num_qubits - 1:
+def entangle_cnot(wires):
+    for i in wires:
+        if i == len(wires) - 1:
             qml.CNOT(wires=(i, 0))
         else:
             qml.CNOT(wires=(i, i+1))
 
 
-def entangle_cz(num_qubits):
-    for i in range(num_qubits):
-        if i is num_qubits - 1:
+def entangle_cz(wires):
+    for i in wires:
+        if i == len(wires) - 1:
             qml.CZ(wires=(i, 0))
         else:
             qml.CZ(wires=(i, i+1))
 
 
-def rotation_layer(parameters, num_qubits):
-    for i in range(num_qubits):
-        qml.RX(parameters[i * num_qubits], wires=i)
-        qml.RZ(parameters[i * num_qubits + 1], wires=i)
-        qml.RX(parameters[i * num_qubits + 2], wires=i)
+def rotation_layer(parameters, wires):
+    for i in range(len(wires)):
+        qml.RX(parameters[i * len(wires)], wires=wires[i])
+        qml.RZ(parameters[i * len(wires) + 1], wires=wires[i])
+        qml.RX(parameters[i * len(wires) + 2], wires=wires[i])
+
+
+def entangling_layers(parameters, layers, wires):
+    for i in range(parameters / 3 / len(wires)):
+        rotation_layer(parameters, wires)
+
+
+def composer(*args):
+    def new_func(features, wires):
+        for arg in args:
+            arg(features, wires)
+    return new_func
--- quantum/Quantum.py
+++ quantum/Quantum.py
@@ -1,20 +1,26 @@
 import pennylane as qml
 from sklearn.metrics import mean_squared_error
-
+from scipy.optimize import minimize
 
 class QuantumRegressor:
-    def __init__(self, encoder, variational, num_qubits, device='default.qubit',
-                max_iterations=100):
+    def __init__(
+            self,
+            encoder,
+            variational,
+            num_qubits,
+            optimizer='COBYLA',
+            device='default.qubit'):
+        self._set_device(device, num_qubits)
         self.x = None
         self.y = None
         self.params = None
         self.encoder = encoder
         self.variational = variational
         self.num_qubits = num_qubits
-        self.device = qml.device(device, wires=num_qubits)
-        self.max_iterations = max_iterations
-        self.optimizer = qml.SPSAOptimizer(maxiter=max_iterations)
         self.qnode = qml.QNode(self._circuit, self.device)
+
+    def _set_device(self, device, num_qubits):
+        self.device = qml.device(device, wires=num_qubits)
 
     def _circuit(self, features, parameters):
         self.encoder(features, wires=range(self.num_qubits))
@@ -25,18 +31,18 @@ class QuantumRegressor:
         predicted_y = [self.qnode(x, parameters) for x in self.x]
         return mean_squared_error(self.y, predicted_y)
 
-    def fit(self, x, y, initial_parameters=None):
+    def fit(self, x, y, initial_parameters=None, detailed_results=False):
         if initial_parameters is None:
-            pass
+            raise ValueError('Missing initial parameters')  # to do, assign a random set of initial parameters if
+            # none are passed
         self.x = x
         self.y = y
         params = initial_parameters
-        cst = []
-        for _ in range(self.max_iterations):
-            params, cost = self.optimizer.step_and_cost(self._cost, params)
-            cst.append(cost)
-        self.params = params
-        return params, cst
+        opt_result = minimize(self._cost, x0=params, method='COBYLA')
+        self.params = opt_result['x']
+        if detailed_results:
+            return opt_result
+        return self.params
 
     def predict(self, x):
         if self.params is None:
