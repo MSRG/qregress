f0e7bebf8e5682b2ca6610d4f5dc3b3823a78d45
taylo773 <nick.taylor@mail.utoronto.ca>
2023-06-29 11:21:35 -0400

    Moved stuff out of main

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import joblib
 import matplotlib.pyplot as plt
-import numpy as np
 import click
 import json
-import pandas
+import time
 from quantum.Quantum import QuantumRegressor
 from quantum.circuits.Encoders import double_angle, single_angle, iqp_embedding, mitarai, composer, \
     entangle_cz, entangle_cnot
@@ -11,29 +10,34 @@ from quantum.circuits.Ansatz import HardwareEfficient, EfficientSU2, TwoLocal, E
     RealAmplitudes, HadamardAnsatz, ModifiedPauliTwo, NLocal
 
 from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
 
 # Global variables
 OPTIMIZER = None
 SHOTS = None
-LOAD_FILE = None
-LENGTH = None
 X_DIM = None
 BACKEND = None
 DEVICE = None
 SCALE_FACTORS = None
+ANSATZ = None
+ENCODER = None
+POSTPROCESS = None
+ERROR_MITIGATION = None
 
 
-ERROR_MITIGATION = [
+############################################
+#  Lists of acceptable values
+############################################
+
+
+ERROR_MITIGATION_LIST = [
+    None,
     'MITIQ_LINEAR',
     'MITIQ_Richardson',
     'M3',
     'TREX'
 ]
 
-
-ENCODERS = {
+ENCODER_LIST = {
     'M': mitarai,
     'A1': single_angle,
     'A2': double_angle,
@@ -51,11 +55,16 @@ ENCODERS = {
 }
 
 # TODO: Create a full list of ansatz to be used in the experiment
-ANSATZES = {
-    'HardwareEfficient': None
+ANSATZ_LIST = {
+    'HardwareEfficient': HardwareEfficient()
 }
 
-MEASUREMENTS = [None, 'simple', 'ridge', 'lasso']
+POSTPROCESS_LIST = {
+    'None': None,
+    'simple': 'simple',
+    'ridge': 'ridge',
+    'lasso': 'lasso'
+}
 
 
 ############################################
@@ -73,15 +82,6 @@ def parse_settings(settings_file):
     global SHOTS
     SHOTS = settings['SHOTS']
 
-    global LOAD_FILE
-    LOAD_FILE = settings['LOAD_FILE']
-
-    global LENGTH
-    LENGTH = settings['LENGTH']
-
-    global X_DIM
-    X_DIM = settings['X_DIM']
-
     global BACKEND
     BACKEND = settings['BACKEND']
 
@@ -91,35 +91,23 @@ def parse_settings(settings_file):
     global SCALE_FACTORS
     SCALE_FACTORS = settings['SCALE_FACTORS']
 
-############################################
-# Dataset preparation
-############################################
+    global ANSATZ
+    ANSATZ = settings['ANSATZ']
+
+    global ENCODER
+    ENCODER = settings['ENCODER']
 
 
-def load_file(y_label: str = 'BSE'):
-    length = LENGTH
-    df = joblib.load(LOAD_FILE)
-    if length is None:
-        length = len(df[y_label])
-    y = df[y_label][:length]
-    y = np.array(y)
-    x = df.drop([y_label], axis=1)[:length]
-    return x, y
+def load_dataset(file):
+    print(f'Loading dataset from {file}... ')
+    data = joblib.load(file)
+    X = data['X']
+    y = data['y']
 
-
-def split(x, y, test_ratio: float = 0.2, x_dim: int = None):
-    if x_dim is not None:
-        raise NotImplementedError('PCA / component analysis is not yet implemented.')
-    scaler = MinMaxScaler
-    x_scaler = scaler((-1, 1))
-    y_scaler = scaler((-1, 1))
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=test_ratio)
-    sc_X_tr = x_scaler.fit_transform(X_train)
-    sc_X_te = x_scaler.transform(X_test)
-    sc_y_tr = y_scaler.fit_transform(y_train).reshape(-1)
-    sc_y_te = y_scaler.transform(y_test).reshape(-1)
-
-    return sc_X_tr, sc_y_tr, sc_X_te, sc_y_te  # returns (X_train y_train X_test y_test)
+    global X_DIM
+    _, X_DIM = X.shape
+    print(f'Successfully loaded {file} into X and y data. ')
+    return X, y
 
 
 ############################################
@@ -127,53 +115,30 @@ def split(x, y, test_ratio: float = 0.2, x_dim: int = None):
 ############################################
 
 @click.command()
-@click.option('--test_ratio', default=0.2, help='Ratio of data to use for testing')
-@click.option('--x_dim', default=16, help='Number of features to reduce X dataset to')
-@click.option('--ansatz', default=None, help='Optionally specify a specific ansatz to run')
-@click.option('--encoder', default=None, help='Optionally specify a specific encoder circuit to run')
-@click.option('--measurement', default=None, help='Optionally specify a specific classical postprocessing')
-@click.option('--error_mitigation', default=None, help='Specify error mitigation to be used, defaults to None')
-@click.option('--settings', required=True, help='Settings file for running ML')
-def main(test_ratio, x_dim, new_ansatz, new_encoder, measurement, error_mitigation, settings):
+@click.option('--settings', required=True, help='Settings file for running ML. ')
+@click.option('--train_set', required=True, help='Datafile for training the ML model. ')
+@click.option('--test_set', default=None, help='Optional datafile to use for testing and scoring the model. ')
+def main(settings, train_set, test_set):
+    X_train, y_train = load_dataset(train_set)
+    parse_settings(settings)
 
-    parse_settings(settings_file=settings)
-    X, y = load_file()
-    X_train, y_train, X_test, y_test = split(X, y, test_ratio, x_dim)
-    num_qubits = x_dim
+    print(f'Creating and training model with given dataset {test_set} at time {time.asctime()}. ')
+    st = time.time()
+    model = create_model()
+    model.fit(X_train, y_train, callback_interval=1)
+    et = time.time()
+    print(f'Training complete taking {st-et} total seconds. ')
 
-    if new_encoder is None:
-        encoders = ENCODERS
-    else:
-        encoders = {new_encoder, ENCODERS[new_encoder]}
-    if new_ansatz is None:
-        ansatzes = ANSATZES
-    else:
-        ansatzes = {new_ansatz, ANSATZES[new_ansatz]}
-    if measurement is None:
-        measurements = MEASUREMENTS
-    else:
-        if measurement == 'None':
-            measurements = [None]
-        else:
-            measurements = measurement
-
-    scores = {}
-    for encoder_name, encoder in encoders.values():
-        for ansatz_name, ansatz in ansatzes.values():
-            for measurement in measurements:
-                model = create_model(encoder, ansatz, measurement, num_qubits, error_mitigation)
-                model.fit(X_train, y_train, callback_interval=1)
-                score = evaluate(model, X_train, X_test, y_train, y_test, plot=True, title=encoder_name + ansatz_name)
-                scores[encoder_name+'_'+ansatz_name+'_'+measurement] = score
-    return scores
+    if test_set is not None:
+        X_test, y_test = load_dataset(test_set)
+        evaluate(model, X_train, X_test, y_train, y_test, plot=True)
 
 
-def create_model(encoder, variational, measurement, num_qubits, error_mitigation):
-    if error_mitigation not in ERROR_MITIGATION:
-        raise ValueError(f'Unexpected type for error mitigation. Type must be in {ERROR_MITIGATION}')
+def create_model():
+    ANSATZ.set_wires(range(X_DIM))
 
-    model = QuantumRegressor(encoder, variational, num_qubits=num_qubits, optimizer=OPTIMIZER, device=DEVICE,
-                             backend=BACKEND, postprocess=measurement, error_mitigation=error_mitigation)
+    model = QuantumRegressor(encoder=ENCODER, variational=ANSATZ, num_qubits=X_DIM, optimizer=OPTIMIZER, device=DEVICE,
+                             backend=BACKEND, postprocess=POSTPROCESS, error_mitigation=ERROR_MITIGATION)
     return model
 
 
