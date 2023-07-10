99e3da1dede42ccba776fb2c9bc0a9168158488c
taylo773 <nick.taylor@mail.utoronto.ca>
2023-07-10 10:54:26 -0400

    Added unscaling the data

--- quantum/Evaluate.py
+++ quantum/Evaluate.py
@@ -1,6 +1,7 @@
 import time
+import numpy as np
 from matplotlib import pyplot as plt
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 
 
 def evaluate_1d(model, X_train, X_test, y_train, y_test, plot=True, title=""):
@@ -21,21 +22,30 @@ def evaluate_1d(model, X_train, X_test, y_train, y_test, plot=True, title=""):
     return scores
 
 
-def evaluate(model, X_train, y_train, X_test=None, y_test=None, plot: bool = False, title: str = 'defult'):
+def evaluate(model, X_train, y_train, X_test=None, y_test=None, plot: bool = False, title: str = 'defult',
+             y_scaler=None):
     scores = {}
     st = time.time()
     print('Now scoring model... ')
-    y_train_pred = model.predict(X_train)
+    y_train_pred = np.array(model.predict(X_train))
+    y_train_pred = y_scaler.inverse_transform(y_train_pred.reshape(-1, 1))
+    y_train = y_scaler.inverse_transform(y_train)
+
     scores['MSE_train'] = mean_squared_error(y_train, y_train_pred),
     scores['R2_train'] = r2_score(y_train, y_train_pred)
+    scores['MAE_train'] = mean_absolute_error(y_train, y_train_pred)
 
     y_test_pred = None
+    y_test = y_scaler.inverse_transform(y_test)
     if y_test is not None:
-        y_test_pred = model.predict(X_test)
+        y_test_pred = np.array(model.predict(X_test))
+        y_test_pred = y_scaler.inverse_transform(y_test_pred.reshape(-1, 1))
         scores['MSE_test'] = mean_squared_error(y_test, y_test_pred)
         scores['R2_test'] = r2_score(y_test, y_test_pred)
+        scores['MAE_test'] = mean_absolute_error(y_test, y_test_pred)
 
     if plot:
+        plt.figure()
         if y_test_pred is not None:
             plt.scatter(y_test, y_test_pred, color='b', s=10, label='Test')
         plt.scatter(y_train, y_train_pred, color='r', s=10, label='Train')
@@ -43,5 +53,17 @@ def evaluate(model, X_train, y_train, X_test=None, y_test=None, plot: bool = Fal
         plt.xlabel('Actual')
         plt.legend()
         plt.savefig(title+'_plot.svg')
-    print(f'Scoring complete taking {st - time.time()} seconds. ')
-    return scores
+
+        if X_test.shape[1] == 1:
+            plt.figure()
+            plt.title(title)
+            plt.scatter(X_train, y_train_pred, color='b', label='Train', s=10)
+            plt.scatter(X_test, y_test_pred, color='orange', label='Test', s=10)
+            plt.scatter(X_train, y_train, color='green', label='Data', s=10)
+            plt.scatter(X_test, y_test, color='green', s=10)
+            plt.legend()
+            plt.savefig(title+'_1D_plot.svg')
+
+    print(f'Scoring complete taking {time.time() - st} seconds. ')
+
+    return scores, y_test_pred, y_train_pred
