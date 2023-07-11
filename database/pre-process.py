b230d85047f1487a80a0c6ee2d3c0e42ebaa2465
taylo773 <nick.taylor@mail.utoronto.ca>
2023-07-11 14:58:57 -0400

    Added flexibility with specifying split ratios

--- database/pre-process.py
+++ database/pre-process.py
@@ -56,7 +56,7 @@ def shuffle(df: pd.DataFrame, y_label: str, length: Union[int, float]):
     return x, y
 
 
-def split(x, y, x_dim: int, train_ratio: float):
+def split(x, y, x_dim: int, train_ratio: float, test_ratio: float, validate_ratio: float):
     """
     Splits the dataset into train and test sets then scales each to (-1, 1). Then applies PCA
     to reduce the number of features to x_dim.
@@ -68,7 +68,11 @@ def split(x, y, x_dim: int, train_ratio: float):
     x_scaler = scaler((-1, 1))
     y_scaler = scaler((-1, 1))
     X_train, X_test, y_train, y_test = train_test_split(x, y, train_size=train_ratio)
-    X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.5)
+    if validate_ratio != 0:
+        X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=test_ratio/(test_ratio + validate_ratio))
+    else:
+        X_val = np.empty(x.shape)
+        y_val = np.empty(y.shape)
     y_train, y_test, y_val = y_train.reshape(-1, 1), y_test.reshape(-1, 1), y_val.reshape(-1, 1)
     X_train = x_scaler.fit_transform(X_train)
     X_test = x_scaler.transform(X_test)
@@ -89,8 +93,9 @@ def split(x, y, x_dim: int, train_ratio: float):
 
 
 @click.command()
-@click.option('--train_ratio', default=0.8, help='Ratio of dataset to be reserved for training. Must be '
-                                                 'interpretable as a float. ')
+@click.option('--train_ratio', default=0.8, help='Ratio of dataset to be reserved for training. ')
+@click.option('--test_ratio', default=None, type=float, help='Ratio of dataset to reserve for testing. ')
+@click.option('--validate_ratio', default=None, type=float, help='Ratio of dataset to reserve for validation. ')
 @click.option('--x_dim', default=None, type=int, help='Size of feature space to reduce to. ')
 @click.option('--length', default=None, type=float, help='Number of datapoints to be included in the dataset '
                                                          'generation. Can be passed as an int or a ratio. ')
@@ -98,11 +103,23 @@ def split(x, y, x_dim: int, train_ratio: float):
 @click.option('--file', required=True, type=click.Path(exists=True), help='Source file to use for dataset generation. '
                                                                           'Supports either .bin or .csv. ')
 @click.option('--save_name', default=None, type=click.Path(), help='Specify the name of the output files. ')
-def main(train_ratio, x_dim, length, y_label, file, save_name):
+def main(train_ratio, test_ratio, validate_ratio, x_dim, length, y_label, file, save_name):
     """
     Performs PCA dimension reduction to specified size and shuffles the data into specified length. Splits the dataset
     into train and test sets and creates seperate files for each.
     """
+    if test_ratio is None and validate_ratio is None:
+        test_ratio = 1 - train_ratio
+        validate_ratio = 0
+    elif validate_ratio is None:
+        validate_ratio = 1 - train_ratio - test_ratio
+    elif test_ratio is None:
+        test_ratio = 1 - train_ratio - validate_ratio
+
+    ratio_sum = train_ratio + test_ratio + validate_ratio
+    if ratio_sum != 1:
+        raise ValueError(f'Train test validate split must sum to 1 but instead got: {ratio_sum}')
+
     if save_name is None:
         filebase = os.path.basename(file)
         filename, ext = os.path.splitext(filebase)
@@ -110,7 +127,8 @@ def main(train_ratio, x_dim, length, y_label, file, save_name):
 
     df = load_file(file)
     x, y = shuffle(df, y_label, length)
-    X_train, y_train, X_test, y_test, X_val, y_val, y_scaler = split(x, y, x_dim, train_ratio)
+    X_train, y_train, X_test, y_test, X_val, y_val, y_scaler = split(x, y, x_dim, train_ratio, test_ratio,
+                                                                     validate_ratio)
 
     train = {
         'X': X_train,
@@ -133,10 +151,12 @@ def main(train_ratio, x_dim, length, y_label, file, save_name):
 
     joblib.dump(train, train_name)
     joblib.dump(test, test_name)
-    joblib.dump(validate, validate_name)
+    if validate_ratio != 0:
+        joblib.dump(validate, validate_name)
     joblib.dump(y_scaler, scaler_name)
 
-    print(f'Successfully created outfiles as {train_name}, {test_name} and {validate_name}. ')
+    print(f'Successfully created outfiles as {train_name}, {test_name}'
+          f'{f" and {validate_name}. " if validate_ratio != 0 else ". "}')
 
 
 if __name__ == '__main__':
