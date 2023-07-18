29a9d098e0d11260c04e2c4cfffe979b77a10ad8
taylo773 <nick.taylor@mail.utoronto.ca>
2023-07-18 12:09:11 -0400

    Added UMAP/TSNE for reduction as well as plotting

--- database/pre-process.py
+++ database/pre-process.py
@@ -1,13 +1,22 @@
 import click
 import joblib
 import os
+import umap
 import numpy as np
 import pandas as pd
 from typing import Union
+from matplotlib import pyplot as plt
+import seaborn as sns
 
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+
+
+############################################
+# Load and Split
+############################################
 
 
 def load_file(file: str):
@@ -56,13 +65,14 @@ def shuffle(df: pd.DataFrame, y_label: str, length: Union[int, float]):
     return x, y
 
 
-def split(x, y, x_dim: int, train_ratio: float, test_ratio: float, validate_ratio: float):
+def split(x, y, train_ratio: float, test_ratio: float, validate_ratio: float):
     """
     Splits the dataset into train and test sets then scales each to (-1, 1). Then applies PCA
     to reduce the number of features to x_dim.
     """
 
     print('Now splitting and scaling data... ')
+    # I don't know if we need to create a validate set here or if it should be done somewhere else.
 
     scaler = MinMaxScaler
     x_scaler = scaler((-1, 1))
@@ -82,14 +92,75 @@ def split(x, y, x_dim: int, train_ratio: float, test_ratio: float, validate_rati
     y_val = y_scaler.transform(y_val)
     print(f'Data successfully split into a {train_ratio} train ratio and scaled to {(-1, 1)} ')
 
-    if x_dim is not None:
-        print(f'Now applying PCA to reduce to {x_dim} features... ')
-        pca = PCA(n_components=x_dim, svd_solver='full')
-        X_train = pca.fit_transform(X_train)
-        X_test = pca.transform(X_test)
-        print(f'Successfully reduced the dataset to {x_dim} features. ')
     return X_train, y_train, X_test, y_test, X_val, y_val, y_scaler  # returns (X_train y_train X_test y_test X_val
     # y_val, y_scaler)
+
+
+############################################
+# Dimension Reduction
+############################################
+# TODO: Change from using PCA to using UMAP / TSNE
+
+dim_methods_list = ['UMAP', 'TSNE', 'PCA']
+
+
+def dim_reduction(X_train, X_test, X_val, x_dim: int, method: str):
+    dim_methods = {
+        'UMAP': umap.UMAP(n_components=x_dim),
+        'TSNE': TSNE(n_components=x_dim, perplexity=50),
+        'PCA': PCA(n_components=x_dim, svd_solver='full')
+    }
+    print(f'Now applying {method} to reduce to {x_dim} features... ')
+    reducer = dim_methods[method]
+    X_train = reducer.fit_transform(X_train)
+    X_test = reducer.fit_transform(X_test)
+    print(f'Successfully reduced train and test. ')
+    if not (X_val.base is None):
+        print('is inside')
+        X_val = reducer.fit_transform(X_val)
+    print(f'Successfully reduced the dataset to {x_dim} features. ')
+
+    return X_train, X_test, X_val
+
+
+def plot_dimension(X_train, X_test, X_val, y_train, y_test, y_val):
+    methods = dim_methods_list
+    datasets = {
+        'Train': (X_train, y_train),
+        'Test': (X_test, y_test),
+        'Validation': (X_val, y_val)
+    }
+
+    plt.figure(figsize=(15, 9))
+    for i, method in enumerate(methods):
+        for j, (dataset, (X_data, y_data)) in enumerate(datasets.items()):
+            if dataset == 'Validation' and X_val.base is None:
+                continue
+
+            if dataset == 'Train':
+                X_train_reduced, _, _ = dim_reduction(X_train, X_test, X_val, x_dim=2, method=method)
+                X_data_reduced = X_train_reduced
+            elif dataset == 'Test':
+                _, X_test_reduced, _ = dim_reduction(X_train, X_test, X_val, x_dim=2, method=method)
+                X_data_reduced = X_test_reduced
+            else:
+                _, _, X_val_reduced = dim_reduction(X_train, X_test, X_val, x_dim=2, method=method)
+                X_data_reduced = X_val_reduced
+
+            ax = plt.subplot(len(methods), len(datasets), i * len(datasets) + j + 1)
+            ax.scatter(X_data_reduced[:, 0], X_data_reduced[:, 1], c=y_data, cmap='coolwarm', s=1)
+            ax.set_title(f'{method} - {dataset} Data')
+            ax.set_xlabel('Feature 1')
+            ax.set_ylabel('Feature 2')
+            plt.colorbar(ax.scatter(X_data_reduced[:, 0], X_data_reduced[:, 1], c=y_data, cmap='coolwarm', s=1), ax=ax)
+
+    plt.tight_layout()
+    plt.show()
+    exit()
+
+############################################
+# Main
+############################################
 
 
 @click.command()
@@ -100,10 +171,14 @@ def split(x, y, x_dim: int, train_ratio: float, test_ratio: float, validate_rati
 @click.option('--length', default=None, type=float, help='Number of datapoints to be included in the dataset '
                                                          'generation. Can be passed as an int or a ratio. ')
 @click.option('--y_label', default='BSE', help='Specify the label of the column to use as target values. ')
+@click.option('--dimension_analysis', default='UMAP', type=click.Choice(dim_methods_list),
+              help=f'Specify the method to reduce the feature space. ')
+@click.option('--visualize', default=False, help='Generates figures of the dimensionality reduction. ')
 @click.option('--file', required=True, type=click.Path(exists=True), help='Source file to use for dataset generation. '
                                                                           'Supports either .bin or .csv. ')
 @click.option('--save_name', default=None, type=click.Path(), help='Specify the name of the output files. ')
-def main(train_ratio, test_ratio, validate_ratio, x_dim, length, y_label, file, save_name):
+def main(train_ratio, test_ratio, validate_ratio, x_dim, length, y_label, file, save_name, dimension_analysis,
+         visualize):
     """
     Performs PCA dimension reduction to specified size and shuffles the data into specified length. Splits the dataset
     into train and test sets and creates seperate files for each.
@@ -127,8 +202,12 @@ def main(train_ratio, test_ratio, validate_ratio, x_dim, length, y_label, file, 
 
     df = load_file(file)
     x, y = shuffle(df, y_label, length)
-    X_train, y_train, X_test, y_test, X_val, y_val, y_scaler = split(x, y, x_dim, train_ratio, test_ratio,
+    X_train, y_train, X_test, y_test, X_val, y_val, y_scaler = split(x, y, train_ratio, test_ratio,
                                                                      validate_ratio)
+    if x_dim is not None:
+        if visualize:
+            plot_dimension(X_train, X_test, X_val, y_train, y_test, y_val)
+        X_train, X_test, X_val = dim_reduction(X_train, X_test, X_val, x_dim, dimension_analysis)
 
     train = {
         'X': X_train,
