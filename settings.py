07e7497435f5d8b7a67b966a0818b3fcb59da4b8
taylo773 <nick.taylor@mail.utoronto.ca>
2023-08-02 14:13:54 -0400

    Fixed hyperparameter setting bug

--- settings.py
+++ settings.py
@@ -75,7 +75,7 @@ def create_settings(filename: str, settings: dict, postprocess, error_mitigation
     # check post-process, if None then reduce hyperparameter search space to decrease redundancies and
     # improve train time. Alpha and beta parameters are not used when post-process is None so are set to [0] as to not
     # iterate over the same settings during grid-search.
-    if postprocess is None:
+    if postprocess is None or postprocess == 'None':
         hyperparameters['alpha'] = [0]
         hyperparameters['beta'] = [0]
     settings['POSTPROCESS'] = POSTPROCESS_LIST[postprocess]
