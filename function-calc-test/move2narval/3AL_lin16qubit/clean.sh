6ee1de226876eadc3c18cb1fc48ebb1df5474b48
Grier Jones <gjones39@vols.utk.edu>
2024-05-13 15:11:19 -0400

    Move re-upload depth and ansatz layer testing to narval

--- function-calc-test/move2narval/3AL_lin16qubit/clean.sh
+++
@@ -1,5 +0,0 @@
-find . -name '*svg' -exec rm {} \;
-find . -name '*csv' -exec rm {} \;
-find . -name '*bin' -exec rm {} \;
-find . -name '*results.json' -exec rm {} \;
-find . -name '*out' -exec rm {} \;
