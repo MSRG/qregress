62d4ad18bdc97a896d174287e3429f3e5cc3adce
ChemRacer <grier_jones@yahoo.com>
2024-07-03 17:01:23 -0400

    update Wed  3 Jul 2024 17:01:23 EDT

--- BSE_RUD_AL_test/16qubit/3_AL/run.sh
+++ BSE_RUD_AL_test/16qubit/3_AL/run.sh
@@ -4,22 +4,23 @@ for i in */; do
     name=${i%/}
     # Extracting the parent directory name
     settings_folder=${name#M-A1-CNOT_Efficient-CRX_}
-    path="/home/gjones/scratch/BSE-calc-test/16qubit/${name}"
+    path="/home/gjones/scratch/BSE_RUD_AL_test/16qubit/3_AL/${name}"
     if [ ! -f ${path}/${name}_results.json ]; then
         echo "${name}.done not found!"
     	cat > ${name}.sub <<EOF
 #! /bin/bash
 #SBATCH -t 7-00:00:00
-#SBATCH -J PCA16_Morgan_${name}
+#SBATCH -J 3_AL_PCA16_Morgan_${name}
 #SBATCH -N 1
 #SBATCH -n 16
+#SBATCH --mem-per-cpu=260M
 #SBATCH --account=rrg-jacobsen-ab
 #SBATCH --error=${name}.e%J        # The file where run time errors will be dumped
 #SBATCH --output=${name}.o%J               # The file where the output of the terminal will be dumped
 
 module load apptainer 
-cd /home/gjones/scratch/BSE-calc-test/16qubit/$name
-apptainer run -C -B /home/gjones/scratch/BSE-calc-test/16qubit ~/deb.sif /opt/miniconda/bin/python /home/gjones/scratch/BSE-calc-test/16qubit/main.py --save_path ${path}  --settings ${path}/${name}.json --train_set /home/gjones/scratch/BSE-calc-test/16qubit/PCA16_Morgan_train.bin --test_set /home/gjones/scratch/BSE-calc-test/16qubit/PCA16_Morgan_test.bin --scaler /home/gjones/scratch/BSE-calc-test/16qubit/PCA16_Morgan_scaler.bin >> ${name}.out 2>&1 
+cd /home/gjones/scratch/BSE_RUD_AL_test/16qubit/3_AL/$name
+apptainer run -C -B /home/gjones/scratch/BSE_RUD_AL_test/16qubit/3_AL ~/deb.sif /opt/miniconda/bin/python /home/gjones/scratch/BSE_RUD_AL_test/16qubit/3_AL/main.py --save_path ${path}  --settings ${path}/${name}.json --train_set /home/gjones/scratch/BSE_RUD_AL_test/16qubit/3_AL/PCA16_Morgan_train.bin --test_set /home/gjones/scratch/BSE_RUD_AL_test/16qubit/3_AL/PCA16_Morgan_test.bin --scaler /home/gjones/scratch/BSE_RUD_AL_test/16qubit/3_AL/PCA16_Morgan_scaler.bin >> ${name}.out 2>&1 
 
 cd ..
 touch ${name}.done
--- BSE_RUD_AL_test/5qubit/3_AL/run.sh
+++ BSE_RUD_AL_test/5qubit/3_AL/run.sh
@@ -4,13 +4,13 @@ for i in */; do
     name=${i%/}
     # Extracting the parent directory name
     settings_folder=${name#M-A1-CNOT_Efficient-CRX_}
-    path="/home/gjones/scratch/BSE-calc-test/5qubit/${name}"
+    path="/home/gjones/scratch/BSE_RUD_AL_test/5qubit/3_AL/${name}"
     if [ ! -f ${path}/${name}_results.json ]; then
         echo "${name}.done not found!"
     	cat > ${name}.sub <<EOF
 #! /bin/bash
 #SBATCH -t 7-00:00:00
-#SBATCH -J PCA5_Morgan_${name}
+#SBATCH -J 3_AL_PCA5_Morgan_${name}
 #SBATCH -N 1
 #SBATCH -n 16
 #SBATCH --mem-per-cpu=260M
@@ -19,8 +19,8 @@ for i in */; do
 #SBATCH --output=${name}.o%J               # The file where the output of the terminal will be dumped
 
 module load apptainer 
-cd /home/gjones/scratch/BSE-calc-test/5qubit/$name
-apptainer run -C -B /home/gjones/scratch/BSE-calc-test/5qubit ~/deb.sif /opt/miniconda/bin/python /home/gjones/scratch/BSE-calc-test/5qubit/main.py --save_path ${path}  --settings ${path}/${name}.json --train_set /home/gjones/scratch/BSE-calc-test/5qubit/PCA5_Morgan_train.bin --test_set /home/gjones/scratch/BSE-calc-test/5qubit/PCA5_Morgan_test.bin --scaler /home/gjones/scratch/BSE-calc-test/5qubit/PCA5_Morgan_scaler.bin >> ${name}.out 2>&1 
+cd /home/gjones/scratch/BSE_RUD_AL_test/5qubit/3_AL/$name
+apptainer run -C -B /home/gjones/scratch/BSE_RUD_AL_test/5qubit/3_AL ~/deb.sif /opt/miniconda/bin/python /home/gjones/scratch/BSE_RUD_AL_test/5qubit/3_AL/main.py --save_path ${path}  --settings ${path}/${name}.json --train_set /home/gjones/scratch/BSE_RUD_AL_test/5qubit/3_AL/PCA5_Morgan_train.bin --test_set /home/gjones/scratch/BSE_RUD_AL_test/5qubit/3_AL/PCA5_Morgan_test.bin --scaler /home/gjones/scratch/BSE_RUD_AL_test/5qubit/3_AL/PCA5_Morgan_scaler.bin >> ${name}.out 2>&1 
 
 cd ..
 touch ${name}.done
