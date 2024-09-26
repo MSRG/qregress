d86f80f4f98dc8fa5a3e69412e3256e0cd76df21
ChemRacer <grier_jones@yahoo.com>
2024-09-26 17:09:08 -0400

    linear and sine learning curves + manuscript

---
+++ function-calc-test/learning_curves/sine/0.7/add_params.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from glob import glob
+import sys
+import os
+import shutil
+import joblib
+import json
+
+
+# In[2]:
+
+
+for p in glob('*/*.json'):
+    print(p)
+    with open(p,'r') as f:
+        olddt=json.load(f)
+    
+    
+    olddt["BATCH_SIZE"]=25
+    olddt["NUM_CORES"]=36
+    olddt['TOLERANCE']=1e-6
+    olddt['MAX_ITER']=1000
+    print(olddt)    
+
+    with open(p,'w') as g:
+        json.dump(olddt,g)
+
