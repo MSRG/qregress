9c028a239a1bb166e54433c1f916716bbaada87c
Grier Jones <gjones39@vols.utk.edu>
2024-10-03 10:12:49 -0400

    rerun.sh

--- helperfiles/rerun.sh
+++ helperfiles/rerun.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 # Grab iteration informations
-month=$(date +%b)
-raniter=$(cat */model_log.csv  | grep -iE "$month" | tail -n 1  | cut -d',' -f2)
-remain=$((1000-$raniter))
-echo "Iterations left: $remain"
+dir=$(find . -maxdepth 2 -name "partial*")
+#month="Sep"
+#month=$(date +%b)
 
-# Change iterations
-sed -i 's/"MAX_ITER": 1000/"MAX_ITER": '$remain'/g' */*.json
-cat */*.json
 
-# run resubmit.sh
-rm *done *.o* *.e* *sub
-bash run.sh
-rm __pycache__.sub quantum.sub helperfiles.sub
-find . -name "*sub" -exec sbatch {} \;
+# Loop over directories
+for i in $dir; do
+ path=$(dirname $i)
+ echo $path
+ sumiter=0
+ # Loop over existing model_log.csv files to find the final iterations
+ for j in ${path}/*model_log.csv; do
+   echo $j
+   raniter=$(cat $j  | grep -iE "Sep|Oct" | tail -n 1  | cut -d',' -f2)
+   sumiter=$((sumiter + raniter + 1)) 
+   echo "$j has $raniter iterations"
+   echo $sumiter
+ done
+ # Write the remaining iterations to file
+ remain=$((1000 - sumiter))
+ echo "Iterations left: $remain"
+ # mv ${path}/model_log.csv ${path}/1_model_log.csv
+ sed -i 's/"MAX_ITER": 1000/"MAX_ITER": '$remain'/g' ${path}/*.json
+ echo $(cat ${path}/*.json)
+ echo 
+done
+
