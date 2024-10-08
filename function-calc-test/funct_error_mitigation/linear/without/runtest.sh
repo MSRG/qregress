1ce998f1c190ad1f04b360bac4c7e13ed4fec91e
Grier Jones <gjones39@vols.utk.edu>
2024-10-07 21:25:59 -0400

    linear + without

--- function-calc-test/funct_error_mitigation/linear/without/runtest.sh
+++ function-calc-test/funct_error_mitigation/linear/without/runtest.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 main.py --settings IQP_Full-Pauli-CRZ/IQP_Full-Pauli-CRZ.json --train_set linear_train.bin --test_set linear_test.bin --scaler linear_scaler.bin --save_path ./IQP_Full-Pauli-CRZ --save_circuits True 
+python3 main.py --settings IQP_Full-Pauli-CRZ/IQP_Full-Pauli-CRZ.json --train_set linear_train.bin --test_set linear_test.bin --scaler linear_scaler.bin --save_path ./IQP_Full-Pauli-CRZ/ --save_circuits True 
