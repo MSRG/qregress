a8c2da687bf701251d3eea2c45c37aada03031c9
Grier Jones <gjones39@vols.utk.edu>
2024-10-21 21:42:23 -0400

    Mon Oct 21 09:42:23 PM EDT 2024

--- BSE5_real/min_ex.py
+++ BSE5_real/min_ex.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
+
+
+
+#!/usr/bin/env python
+# coding: utf-8
+import sys 
+# !{sys.executable} -m pip install qiskit"[qasm3-import]"
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -17,10 +24,18 @@ import itertools
 import collections.abc
 import pandas as pd
 import pennylane as qml
+from pennylane_qiskit import qiskit_device
 from tqdm import tqdm
 from qiskit_ibm_runtime import QiskitRuntimeService, Session
+from qiskit_ibm_runtime.fake_provider import FakeQuebec
+from qiskit_aer.noise import NoiseModel
+from qiskit_aer import AerSimulator
 
 os.environ["OMP_NUM_THREADS"] = "12"
+
+
+
+
 
 # Set random seed for reproducibility
 np.random.seed(42)
@@ -48,11 +63,30 @@ class RegressionDataset(Dataset):
         return self.X[idx], self.y[idx]
 
 # Create DataLoaders
-batch_size = 32 * 1
+batch_size = None 
 train_dataset = RegressionDataset(X_train, y_train)
 test_dataset = RegressionDataset(X_test, y_test)
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -62,12 +96,22 @@ num_qubits=5
 re_upload_depth=1
 n_layers=1
 
-service = QiskitRuntimeService(channel="ibm_quantum", instance='pinq-quebec-hub/univ-toronto/matterlab')
+service = QiskitRuntimeService(channel="ibm_quantum", 
+                               instance='pinq-quebec-hub/univ-toronto/matterlab'
+                              )
 _backend = service.least_busy(operational=True, simulator=False, min_num_qubits=num_qubits)
-dev = qml.device("qiskit.remote", wires=127, backend=_backend,shots=1024,session=Session(backend=_backend))
 
-# Qulacs simulator
-# dev = qml.device("qulacs.simulator", wires=num_qubits)
+dev = qml.device(
+    "qiskit.remote",
+    wires=127,
+    shots=1024,
+    backend=_backend,
+    resilience_level=1,
+    optimization_level=1,
+    seed_transpiler=42,
+)
+
+
 
 # Simple circuit
 @qml.qnode(dev, interface="torch")
@@ -76,6 +120,10 @@ def circuit(inputs, weights):
         qml.AngleEmbedding(inputs, wires=range(num_qubits))
         qml.BasicEntanglerLayers(weights, wires=range(num_qubits))
     return qml.expval(qml.PauliZ(0))
+
+
+
+
 
 # Initialize pytorch model
 weight_shapes = {"weights": (n_layers, num_qubits)}
@@ -91,54 +139,68 @@ criterion = nn.MSELoss()
 learning_rate = 0.001
 optimizer = optim.Adam(qlayer.parameters(), lr=learning_rate)
 
-# Training loop
-epochs = 10
-train_losses = []
-test_losses = []
-for epoch in tqdm(range(epochs)):
-    model.train()
-    running_loss = 0.0
-    for batch_X, batch_y in tqdm(train_loader):
-        optimizer.zero_grad()
-        batch_X=batch_X.to(device)
-        batch_y=batch_y.to(device)
-        outputs = model(batch_X)
-        loss = criterion(outputs, batch_y)
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item() * batch_X.size(0)
-    
-    epoch_loss = running_loss / len(train_loader.dataset)
-    train_losses.append(epoch_loss)
-    
-    # Evaluation on test set
-    model.eval()
-    test_running_loss = 0.0
-    with torch.no_grad():
-        for batch_X, batch_y in tqdm(test_loader):
+
+
+
+
+
+
+
+with qiskit_device.qiskit_session(dev) as session:
+    print(session.details())
+    # Training loop
+    epochs = 10
+    train_losses = []
+    test_losses = []
+    for epoch in tqdm(range(epochs)):
+        model.train()
+        running_loss = 0.0
+        for batch_X, batch_y in tqdm(train_loader, desc="Train"):
+            optimizer.zero_grad()
+            batch_X=batch_X.to(device)
+            batch_y=batch_y.to(device)
             outputs = model(batch_X)
             loss = criterion(outputs, batch_y)
-            test_running_loss += loss.item() * batch_X.size(0)
-    test_epoch_loss = test_running_loss / len(test_loader.dataset)
-    test_losses.append(test_epoch_loss)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item() * batch_X.size(0)
+        
+        epoch_loss = running_loss / len(train_loader.dataset)
+        train_losses.append(epoch_loss)
+        
+        # Evaluation on test set
+        model.eval()
+        test_running_loss = 0.0
+        with torch.no_grad():
+            for batch_X, batch_y in tqdm(test_loader, desc="Test"):
+                outputs = model(batch_X)
+                loss = criterion(outputs, batch_y)
+                test_running_loss += loss.item() * batch_X.size(0)
+        test_epoch_loss = test_running_loss / len(test_loader.dataset)
+        test_losses.append(test_epoch_loss)
+        
+        if (epoch+1) % 1 == 0 or epoch == 0:
+            print(f"Epoch [{epoch+1}/{epochs}] - Train Loss: {epoch_loss:.4f} - Test Loss: {test_epoch_loss:.4f}")
     
-    if (epoch+1) % 1 == 0 or epoch == 0:
-        print(f"Epoch [{epoch+1}/{epochs}] - Train Loss: {epoch_loss:.4f} - Test Loss: {test_epoch_loss:.4f}")
+    
+    
+    
+    
+    # Evaluation
+    model.eval()
+    predictions = []
+    actuals = []
+    
+    with torch.no_grad():
+        for batch_X, batch_y in train_loader:
+            outputs = model(batch_X)
+            predictions.extend(outputs.squeeze().tolist())
+            actuals.extend(batch_y.squeeze().tolist())
+    
+    
 
 
 
-
-
-# Evaluation
-model.eval()
-predictions = []
-actuals = []
-
-with torch.no_grad():
-    for batch_X, batch_y in train_loader:
-        outputs = model(batch_X)
-        predictions.extend(outputs.squeeze().tolist())
-        actuals.extend(batch_y.squeeze().tolist())
 
 mse = mean_squared_error(actuals, predictions)
 mae = mean_absolute_error(actuals, predictions)
