76a22d8f0e04d863d0f771cf0042ce6bacebac57
ChemRacer <grier_jones@yahoo.com>
2024-11-26 16:42:01 -0500

    Tue 26 Nov 2024 16:42:01 EST

--- function-calc-test/funct_error_mitigation/quadratic/AL5_RUD1/runtest.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export OMP_NUM_THREADS=12
-python3 main.py --settings IQP_Full-Pauli-CRX/IQP_Full-Pauli-CRX.json --train_set quadratic_train.bin --test_set quadratic_test.bin --scaler quadratic_scaler.bin --save_path ./ --save_circuits True  --resume_file final_state_model.bin  
