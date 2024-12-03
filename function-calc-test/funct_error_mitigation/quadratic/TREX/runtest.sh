520ce18eea44b56b704736670374c484ad8ab46d
ChemRacer <grier_jones@yahoo.com>
2024-12-03 17:26:17 -0500

    Qiskit ML

--- function-calc-test/funct_error_mitigation/quadratic/TREX/runtest.sh
+++ function-calc-test/funct_error_mitigation/quadratic/TREX/runtest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export OMP_NUM_THREADS=64
-python3 main.py --settings IQP_Full-Pauli-CRX/IQP_Full-Pauli-CRX.json --train_set quadratic_train.bin --test_set quadratic_test.bin --scaler quadratic_scaler.bin --save_path ./ --save_circuits True # --resume_file final_state_model.bin  
+python3 main.py --settings IQP_Full-Pauli-CRX/IQP_Full-Pauli-CRX.json --train_set quadratic_train.bin --test_set quadratic_test.bin --scaler quadratic_scaler.bin --save_path ./ --resume_file final_state_model.bin  
