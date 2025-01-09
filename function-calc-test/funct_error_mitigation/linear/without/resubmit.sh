8ab86170f3fa39ee97bccaab792a99974e189a65
Grier Jones <gjones39@vols.utk.edu>
2025-01-09 16:57:33 -0500

    Thu Jan  9 04:57:33 PM EST 2025

--- function-calc-test/funct_error_mitigation/linear/without/resubmit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cp -r helperfiles/* .
-rm *done *.o* *.e* *sub
-bash run.sh
-rm __pycache__.sub quantum.sub helperfiles.sub
-find . -name "*sub" -exec sbatch {} \;
---
+++ function-calc-test/funct_error_mitigation/quadratic/Without/IQP_Full-Pauli-CRX/runtest.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export OMP_NUM_THREADS=64
+topdir=$(dirname "$(pwd)")
+python3 ${topdir}/main.py --settings $(pwd)/IQP_Full-Pauli-CRX.json --train_set ${topdir}/quadratic_train.bin --test_set ${topdir}/quadratic_test.bin --scaler ${topdir}/quadratic_scaler.bin --save_path $(pwd) # --resume_file $(pwd)/final_state_model.bin  
