180648d00d459598886a8e613edd9d0a4cf953fb
Grier Jones <gjones39@vols.utk.edu>
2025-02-13 18:15:42 -0500

    Thu Feb 13 06:15:42 PM EST 2025 manuscript + error mitigation

---
+++ Pennylane_to_Qiskit/linear_tests/opt_level/move.sh
@@ -0,0 +1,7 @@
+dir="./3"
+mv partial_state_model.bin ${dir}
+mv model_log.csv ${dir}
+mv IQP_Full-Pauli-CRZ_plot.svg ${dir}
+mv IQP_Full-Pauli-CRZ_1D_plot.svg ${dir}
+mv IQP_Full-Pauli-CRZ_results.json ${dir}
+mv IQP_Full-Pauli-CRZ_predicted_values.csv ${dir}
