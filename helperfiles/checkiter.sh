41e9d01756db457cc65e34f9124300f6d3d07de7
Grier Jones <gjones39@vols.utk.edu>
2025-02-24 15:14:48 -0500

    Mon Feb 24 03:14:48 PM EST 2025

--- helperfiles/checkiter.sh
+++ helperfiles/checkiter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-files=$(find . -name "*model_log.csv")
+files=$(find . -name "model_log.csv" -newermt "$(date +%Y)-$(date +%m)-01")
 for i in $files; do
     echo $i
     echo $(awk -F',' '{print $2}' $i | grep -Eo '[0-9]+' | tail -n 1)
