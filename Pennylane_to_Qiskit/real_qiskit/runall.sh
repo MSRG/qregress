8213fe44c112a06e929d4f3678c3339a3df2b598
ChemRacer <grier_jones@yahoo.com>
2025-03-03 16:08:51 -0500

    Prep real run

--- Pennylane_to_Qiskit/real_qiskit/runall.sh
+++ Pennylane_to_Qiskit/real_qiskit/runall.sh
@@ -3,16 +3,14 @@
 #shots = 1024.0
 #resilience_level = 1
 
-for ol in {1..3};do
- for rl in {1..2};do
-  dirname="ol${ol}_rl${rl}"
-  if [ ! -d ${dirname} ] ; then
-    echo "${dirname} does not exist"
-    mkdir ${dirname}
-  else
-    echo "${dirname} does exist"
-  fi
-  papermill convert_ML_batched.ipynb ${dirname}/convert_ML_batched.ipynb -r optimization_level ${ol} -r resilience_level ${rl}
-  mv ./model_log.csv ./final_state_model.bin ./A2_HWE-CNOT_plot.svg ./A2_HWE-CNOT_results.json ./A2_HWE-CNOT_predicted_values.csv ${dirname}/ 
- done
+for rl in {0..2};do
+ dirname="ol2_rl${rl}"
+ if [ ! -d ${dirname} ] ; then
+   echo "${dirname} does not exist"
+   mkdir ${dirname}
+ else
+   echo "${dirname} does exist"
+ fi
+ papermill convert_ML_batched.ipynb ${dirname}/convert_ML_batched.ipynb -r optimization_level 2 -r resilience_level ${rl} -r shots "6144"
+ mv ./model_log.csv ./final_state_model.bin ./A2_HWE-CNOT_plot.svg ./A2_HWE-CNOT_results.json ./A2_HWE-CNOT_predicted_values.csv ${dirname}/ 
 done
