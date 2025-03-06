8870ff459155cf7dddeafd6e1a69a2f21d15d45f
Grier Jones <gjones39@vols.utk.edu>
2025-03-06 13:52:05 -0500

    DDCC 9RUD + real device opt/res test

--- Pennylane_to_Qiskit/real_qiskit/runall.sh
+++ Pennylane_to_Qiskit/real_qiskit/runall.sh
@@ -4,14 +4,17 @@
 #resilience_level = 1
 
 for rl in {0..2};do
- dirname="ol2_rl${rl}"
- if [ ! -d ${dirname} ] ; then
-   echo "${dirname} does not exist"
-   mkdir ${dirname}
- else
-   echo "${dirname} does exist"
- fi
- papermill convert_ML_batched.ipynb ${dirname}/convert_ML_batched.ipynb -r optimization_level 2 -r resilience_level ${rl} -r shots 3072
- mv ./model_log.csv ./final_state_model.bin ./A2_HWE-CNOT_plot.svg ./A2_HWE-CNOT_results.json ./A2_HWE-CNOT_predicted_values.csv ${dirname}/ 
- mv ./job*txt ${dirname}/ 
+ for ol in {0..3};do
+  dirname="ol${ol}_rl${rl}"
+  if [ ! -d ${dirname} ] ; then
+    echo "${dirname} does not exist"
+    mkdir ${dirname}
+    papermill convert_ML_batched.ipynb ${dirname}/convert_ML_batched.ipynb -r optimization_level 2 -r resilience_level ${rl} -r shots 3072 
+    mv ./model_log.csv ./final_state_model.bin ./A2_HWE-CNOT_plot.svg ./A2_HWE-CNOT_results.json ./A2_HWE-CNOT_predicted_values.csv ${dirname}/ 
+    mv ./job*txt ${dirname}/ 
+  else
+    echo "${dirname} does exist"
+  fi
+ 
+ done
 done
