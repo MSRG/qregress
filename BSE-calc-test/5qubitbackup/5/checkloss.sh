3a810036134ccb52442c3e9d07e6213bcfceffae
ChemRacer <grier_jones@yahoo.com>
2025-03-25 16:24:42 -0400

    5qubit 1000/partial 16 qubit

--- BSE-calc-test/5qubitbackup/5/checkloss.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-dir=$(find . -mindepth 1 -maxdepth 1 -type d)
-for i in $dir; do
- extension=${i#./}
- echo $extension
- if [ -f "${extension}/${extension}_results.json" ]; then
-   python3 view_loss.py --path ${extension}/model_log.csv
- fi
-done  
--- BSE-calc-test/5rerun/clean.sh
+++
@@ -1,6 +0,0 @@
-rm */*.svg
-rm */*model_log.csv
-rm */*_state_model.bin
-rm */*_predicted_values.csv
-rm */*_results.json
-rm */*.out
