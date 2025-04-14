bd3fe1c5c0dfca7305237faad82b6b78a98ac9ae
ChemRacer <grier_jones@yahoo.com>
2025-04-14 17:15:26 -0400

    Mon 14 Apr 2025 17:15:26 EDT

--- Pennylane_to_Qiskit/0.10_run/fake/run.sh
+++ Pennylane_to_Qiskit/0.10_run/fake/run.sh
@@ -5,29 +5,17 @@ for rl in 0 1 2; do
 	dirname="ol${ol}_rl${rl}"
   if [ ! -d $dirname ]; then
 		mkdir $dirname
-	fi
-	cp *bin $dirname
-	cp QiskitRegressor.py $dirname
-	cp fake.py $dirname
-	cd $dirname 
-	sed -i "s/optimization_level = 0/optimization_level = ${ol}/g" fake.py
-	sed -i "s/resilience_level = 2/resilience_level = ${rl}/g" fake.py
-  cat > ${dirname}.sub <<EOF
-#!/bin/bash
-#SBATCH -t 4-00:00:00
-#SBATCH -J ${dirname} 
-#SBATCH -N 1
-#SBATCH --cpus-per-task 64
-#SBATCH --account=rrg-jacobsen-ab
-#SBATCH --error=A2_HWE-CNOT.e%J
-#SBATCH --output=A2_HWE-CNOT.o%J
+  fi
+  cp *bin $dirname
+  cp QiskitRegressor.py $dirname
+  cp fake.py $dirname
+  cd $dirname
 
-export OMP_NUM_THREADS=64
-
-
-/lustre06/project/6006115/gjones/env/bin/python3 fake.py > fake.out 2>&1
-
-EOF
-	cd ../
+  sed -i "s/optimization_level = 0/optimization_level = ${ol}/g" fake.py
+  sed -i "s/resilience_level = 2/resilience_level = ${rl}/g" fake.py
+  export OMP_NUM_THREADS=64
+  echo "Running $dirname using $OMP_NUM_THREADS" 
+  python3 fake.py > fake.out 2>&1
+  cd ../
  done
 done
